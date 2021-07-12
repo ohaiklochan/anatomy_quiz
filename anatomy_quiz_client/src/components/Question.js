3c7386fbb2bf939673f821424d15bcea1dc3cd97
ohaiklochan <59032381+ohaiklochan@users.noreply.github.com>
2021-07-12 12:05:26 -0500

    updated package.json

---
+++ anatomy_quiz_client/src/components/Question.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Card, Header } from 'semantic-ui-react'
+
+const Question = ({question}) => {
+
+  return(
+    <div className="ui center aligned one column grid">
+      <Card.Group style={{'maxWidth':'200px'}} className="ui center aligned one column grid">
+        <Header as="h3">{question.text}</Header>
+        <Card fluid color='purple' header={question.first_answer} />
+        <Card fluid color='orange' header={question.second_answer} />
+        <Card fluid color='blue' header={question.third_answer} />
+        <Card fluid color='green' header={question.fourth_answer} />
+      </Card.Group>
+    </div>
+  )
+}
+
+export default (Question)
